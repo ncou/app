8f66b63a02a00193f0b8d1d7af79fe6d5dfd4062
ncou <nicolas_courade@yahoo.fr>
2020-07-11 11:24:39 +0200

    update

--- config/paths.php
+++ config/paths.php
@@ -4,13 +4,13 @@ $rootPath = dirname(__DIR__, 1);
 
 return [
     '@root'         => $rootPath,
-    '@app'          => '@root/app',
-    '@config'       => '@root/config',
-    '@public'       => '@root/public',
-    '@resources'    => '@root/resources',
-    '@runtime'      => '@root/runtime',
-    '@vendor'       => '@root/vendor',
-    '@views'        => '@resources/views',
-    '@cache'        => '@runtime/cache',
-    '@logs'         => '@runtime/logs',
+    '@app'          => '@root/app/',
+    '@config'       => '@root/config/',
+    '@public'       => '@root/public/',
+    '@resources'    => '@root/resources/',
+    '@runtime'      => '@root/runtime/',
+    '@vendor'       => '@root/vendor/',
+    '@views'        => '@resources/views/',
+    '@cache'        => '@runtime/cache/',
+    '@logs'         => '@runtime/logs/',
 ];
