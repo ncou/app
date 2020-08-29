aa2caeebfa627ffdf22ac31d63105c610044644b
ncou <nicolas_courade@yahoo.fr>
2020-08-29 11:43:09 +0200

    update

---
+++ config/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'debug'    => env('APP_DEBUG', false),
+    'charset'  => env('APP_ENCODING', 'UTF-8'),
+    'locale'   => env('APP_DEFAULT_LOCALE', 'en_US'),
+    'timezone' => env('APP_DEFAULT_TIMEZONE', 'UTC'),
+];
