1c7dbdd4b699a8c61976627c0a5a6883912b1a05
ncou <nicolas_courade@yahoo.fr>
2020-09-26 10:24:41 +0200

    update

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -9,49 +9,27 @@ use Psr\Container\ContainerInterface;
 use Chiron\Views\TemplateRendererInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Chiron\Http\Helper\ResponseCreator;
 
-class HomeController
+final class HomeController
 {
-    /**
-     * Dependency injection container
-     *
-     * @var ContainerInterface
-     */
-    private $container;
+    /** @var TemplateRendererInterface */
+    private $view;
 
-    // TODO : lui passer plutot en paramétre le container, mais dans ce cas il faut pouvoir externaliser le container !!!!
-    public function __construct(ContainerInterface $container)
+    public function __construct(TemplateRendererInterface $view)
     {
-        $this->container = $container;
-        // TODO : initialiser la view/renderer directement dans le fichier "dependencies.php" ?????
-        $this->view = $container->get(TemplateRendererInterface::class);
+        $this->view = $view;
     }
 
-    public function index(ServerRequestInterface $request): ResponseInterface
+    public function index(ResponseCreator $responder): ResponseInterface
     {
         $name = "FOOBAR";
 
         $this->view->addAttribute('name', $name);
 
         $content = $this->view->render('hello');
-        $response = $this->createResponse($content, 200, ['Content-Type' => 'text/html']);
 
-        return $response;
-    }
-
-    // TODO : vérifier que cela ne pose pas de problémes si on passe un content à null, si c'est le cas initialiser ce paramétre avec chaine vide.
-    private function createResponse(string $content = null, int $statusCode = 200, array $headers = []): ResponseInterface
-    {
-        $responseFactory = $this->container->get('Psr\Http\Message\ResponseFactoryInterface');
-        $response = $responseFactory->createResponse($statusCode);
-
-        foreach ($headers as $name => $value) {
-            $response = $response->withHeader($name, $value);
-        }
-
-        $response->getBody()->write($content);
-
-        return $response;
+        return $responder->html($content);
     }
 }
 
