a7e8e530fee44c2bb9a10bea9206bdda1f52435c
ncou <nicolas_courade@yahoo.fr>
2020-11-05 00:06:43 +0100

    update

--- app/Bootloader/AddMiddlewareBootloader.php
+++ app/Bootloader/AddMiddlewareBootloader.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Bootloader;
 
 use Chiron\Bootload\AbstractBootloader;
@@ -12,6 +14,4 @@ class AddMiddlewareBootloader extends AbstractBootloader
     {
         $middlewares->addMiddleware(XClacksOverheadMiddleware::class);
     }
-
 }
-
