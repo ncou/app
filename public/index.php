e1e78f511b70f0ba9f2700674958f4ee2e17be5c
ncou <nicolas_courade@yahoo.fr>
2021-03-13 16:24:26 +0100

    update

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@ $currentDir = PHP_SAPI === 'cli' ? getcwd() : realpath(getcwd().'/../');
 require $currentDir . '/bootstrap/requirements.php';
 require $currentDir . '/vendor/autoload.php';
 
+Chiron\Debug\Debug::enable();
+
 $paths = require $currentDir . '/bootstrap/paths.php';
 $app = Chiron\Application::init($paths);
 
