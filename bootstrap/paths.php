2a4120f580083c1d1298ba49de0807df41f30f9e
ncou <nicolas_courade@yahoo.fr>
2021-03-13 15:47:22 +0100

    update

--- bootstrap/paths.php
+++ bootstrap/paths.php
@@ -3,13 +3,13 @@
 $rootPath = dirname(__DIR__, 1);
 
 return [
-    '@root'         => $rootPath,
-    '@app'          => '@root/app/',
-    '@config'       => '@root/config/',
-    '@public'       => '@root/public/',
-    '@resources'    => '@root/resources/',
-    '@runtime'      => '@root/runtime/',
-    '@vendor'       => '@root/vendor/',
-    '@views'        => '@resources/views/',
-    '@cache'        => '@runtime/cache/',
+    '@root'      => $rootPath,
+    '@app'       => '@root/app/',
+    '@config'    => '@root/config/',
+    '@public'    => '@root/public/',
+    '@resources' => '@root/resources/',
+    '@runtime'   => '@root/runtime/',
+    '@vendor'    => '@root/vendor/',
+    '@views'     => '@resources/views/',
+    '@cache'     => '@runtime/cache/',
 ];
