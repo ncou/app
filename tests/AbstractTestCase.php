2a5a518fbc7694711e69ab2fe5c248c4147aa94e
ncou <nicolas_courade@yahoo.fr>
2021-03-13 13:58:37 +0100

    update

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -68,6 +68,7 @@ abstract class AbstractTestCase extends BaseTestCase
     }
 
     // TODO : attacher automatiquement le base_path à l'uri !!!
+    // https://github.com/clue/reactphp-buzz/blob/2d4c93be8cba9f482e96b8567916b32c737a9811/src/Message/MessageFactory.php#L120
     protected function request(string $method, $uri, array $serverParams = []): ServerRequestInterface
     {
         return new ServerRequest($method, $uri, [], null, '1.1', $serverParams);
