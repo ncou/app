aa01dc47ae7b51d140b23dfcee1b950b214ed218
ncou <nicolas_courade@yahoo.fr>
2021-07-10 10:54:42 +0200

    update

--- app/Installer.php
+++ app/Installer.php
@@ -9,6 +9,13 @@ use FilesystemIterator;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 
+//https://github.com/cakephp/app/blob/master/src/Console/Installer.php
+//https://github.com/craftcms/cms/blob/develop/bootstrap/bootstrap.php#L50
+//https://github.com/prefeiturasp/minuta-participativa/blob/731ed5ebd3f3cbdb323ff14c3777fd221e68f64c/scripts/Bedrock/Installer.php#L40
+//https://github.com/pixelfed/pixelfed/blob/f5260902293b35c830c865f5bc88162ae6e60f57/app/Console/Commands/Installer.php
+
+//https://github.com/laravel/framework/blob/8.x/src/Illuminate/Foundation/ComposerScripts.php
+
 final class Installer
 {
     public static function postUpdate(Event $event = null): void
