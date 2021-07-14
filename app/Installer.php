8c80d0928ab6b841f5ca04cec07c702bbbf1c009
ncou <nicolas_courade@yahoo.fr>
2021-07-14 11:30:23 +0200

    update

--- app/Installer.php
+++ app/Installer.php
@@ -9,6 +9,18 @@ use FilesystemIterator;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 
+// TODO : Exemple de fichiers composer ou on appel dans un certain ordre des commandes pour préparer l'application :
+// ordre des commandes : https://getcomposer.org/doc/articles/scripts.md#command-events
+//https://github.com/top-think/think/blob/6.0/composer.json
+//https://github.com/spiral/app/blob/master/composer.json
+//https://github.com/ventoviro/windwalker-starter/blob/master/composer.json
+//https://github.com/yiisoft/demo/blob/master/composer.json
+//https://github.com/symfony/demo/blob/main/composer.json
+//https://github.com/laravel/laravel/blob/8.x/composer.json
+
+//https://github.com/ventoviro/windwalker-core/blob/master/src/Core/Composer/StarterInstaller.php#L72
+//https://github.com/ventoviro/windwalker-starter/blob/master/composer.json#L31
+
 //https://github.com/cakephp/app/blob/master/src/Console/Installer.php
 //https://github.com/craftcms/cms/blob/develop/bootstrap/bootstrap.php#L50
 //https://github.com/prefeiturasp/minuta-participativa/blob/731ed5ebd3f3cbdb323ff14c3777fd221e68f64c/scripts/Bedrock/Installer.php#L40
@@ -18,7 +30,7 @@ use RecursiveIteratorIterator;
 
 final class Installer
 {
-    public static function postUpdate(Event $event = null): void
+    public static function prepareFolders(Event $event = null): void
     {
         self::chmodRecursive('runtime', 0777);
         self::chmodRecursive('public/assets', 0777);
@@ -37,5 +49,15 @@ final class Installer
         foreach ($iterator as $item) {
             chmod($item, $mode);
         }
+    }
+
+
+    public static function thanksReminder(Event $event = null): void
+    {
+        $io = $event->getIO();
+
+        $io->writeError('');
+        $io->writeError('Try running the command <comment>bin/chiron thanks</> to spread some love!</>');
+        $io->writeError('');
     }
 }
